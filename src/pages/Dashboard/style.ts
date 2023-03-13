8e0079235ca3d79eccf7ebf73b9a72f33ba0d756
Eduardo Kucek <47871987+eduardokucek@users.noreply.github.com>
2023-03-13 14:09:52 -0300

    Revert "Feature/dashboard"

--- src/pages/Dashboard/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const DashboardStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* height: 100%;
-  width: 100%; */
-  background-color: #d9d9d9;
-`;
